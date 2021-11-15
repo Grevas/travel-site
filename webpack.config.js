a6864478b2f69105100e52ff2f57236d2ed3f850
Grevas <Grevas@yandex.ru>
2021-11-15 18:29:28 +0300

    completed large hero block and intro content block.

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./app/assets/scripts/App.js",
+  output: {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app"),
+  },
+  devServer: {
+    static: [path.join(__dirname, "app")],
+    hot: true,
+    port: 3000,
+    host: "0.0.0.0",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: "postcss-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
