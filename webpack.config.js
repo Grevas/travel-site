5885165a08bb43a9b586325f99cb9225bdb45375
Grevas <Grevas@yandex.ru>
2021-11-25 20:46:41 +0300

    Completed our build process configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,109 @@
+const currentTask = process.env.npm_lifecycle_event;
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const fse = require("fs-extra");
 
-module.exports = {
+class RunAfterCompile {
+  apply(compiler) {
+    compiler.hooks.done.tap("Copy images", function () {
+      fse.copySync("./app/assets/images", "./docs/assets/images");
+    });
+  }
+}
+
+let cssConfig = {
+  test: /\.css$/i,
+  use: [
+    {
+      loader: "css-loader",
+      options: {
+        importLoaders: 1,
+      },
+    },
+    {
+      loader: "postcss-loader",
+    },
+  ],
+};
+
+let pages = fse
+  .readdirSync("./app")
+  .filter(function (file) {
+    return file.endsWith(".html");
+  })
+  .map(function (page) {
+    return new HtmlWebpackPlugin({
+      filename: page,
+      template: `./app/${page}`,
+    });
+  });
+
+let config = {
   entry: "./app/assets/scripts/App.js",
-  output: {
+  plugins: pages,
+  module: {
+    rules: [cssConfig],
+  },
+};
+
+if (currentTask == "dev") {
+  cssConfig.use.unshift("style-loader");
+  config.output = {
     filename: "bundled.js",
     path: path.resolve(__dirname, "app"),
-  },
-  devServer: {
+  };
+  config.devServer = {
     static: [path.join(__dirname, "app")],
     hot: true,
     port: 3000,
     host: "0.0.0.0",
-  },
+  };
+  config.mode = "development";
+}
+
+if (currentTask == "build") {
+  config.module.rules.push({
+    test: /\.js$/,
+    exclude: /(node_modules)/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+      },
+    },
+  });
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader);
+  config.output = {
+    filename: "[name].[chunkhash].js",
+    chunkFilename: "[name].[chunkhash].js",
+    path: path.resolve(__dirname, "docs"),
+  };
+  config.mode = "production";
+  config.optimization = {
+    splitChunks: { chunks: "all" },
+  };
+  config.plugins.push(
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: "styles.[chunkhash].css" }),
+    new RunAfterCompile()
+  );
+}
+
+module.exports = config;
+
+/* module.exports = {
+
+   devServer: {
+         static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    static: [path.join(__dirname, "app")],
+    hot: true,
+    port: 3000,
+    host: "0.0.0.0",
+  }, 
   mode: "development",
   module: {
     rules: [
@@ -35,4 +127,4 @@ module.exports = {
       },
     ],
   },
-};
+};*/
